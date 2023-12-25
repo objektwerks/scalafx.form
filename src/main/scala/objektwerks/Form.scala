f9dc121c8284d6407bd69620cf863d7c9cf00a73
objektwerks <objektwerks@runbox.com>
2023-12-25 13:23:24 -0500

    refactored form

--- src/main/scala/objektwerks/Form.scala
+++ src/main/scala/objektwerks/Form.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import scalafx.geometry.{Insets, Pos}
+import scalafx.geometry.Insets
 import scalafx.scene.control.Label
 import scalafx.scene.layout.{GridPane, Region}
 
@@ -8,17 +8,13 @@ final case class Validator()
 
 final case class Field(label: Label, component: Region, validator: Validator)
 
-final case class Fields(fields: List[Field])
-
-final class Form(controls: List[(String, Region)]) extends GridPane:
+final class Form(fields: List[Field]) extends GridPane:
   hgap = 6
   vgap = 6
   padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
   var row = 0
-  for ((label, control) <- controls)
-    val columnLabel = new Label:
-      alignment = Pos.CenterLeft
-      text = label
+  for (field <- fields)
+    val columnLabel = field.label
     add(columnLabel, columnIndex = 0, rowIndex = row)
-    add(control, columnIndex = 1, rowIndex = row)
+    add(field.component, columnIndex = 1, rowIndex = row)
     row += 1
