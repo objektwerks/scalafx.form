67d3618cb185af9dfe4063ab8fc85eb5321a424c
objektwerks <objektwerks@runbox.com>
2023-12-27 12:21:06 -0500

    edited popup view

--- src/main/scala/objektwerks/DateTimeField.scala
+++ src/main/scala/objektwerks/DateTimeField.scala
@@ -45,9 +45,9 @@ final class DateTimeField(localDateTime: LocalDateTime) extends HBox:
 private final class PopupView(localDateTime: LocalDateTime,
                               popup: Popup,
                               popupValue: (LocalDateTime) => Unit) extends VBox: 
-  spacing = 6
-  padding = Insets(6)
   alignment = Pos.CENTER
+  padding = Insets(6)
+  spacing = 6
   style = "-fx-background-color:white;-fx-border-color: gray;-fx-border-width:2;-fx-border-radius:3;-fx-hgap:3;-fx-vgap:3;"
   val yearSpinner = Spinner[Int](min = localDateTime.getYear - 1, max = localDateTime.getYear, initialValue = localDateTime.getYear, amountToStepBy = 1)
   val monthSpinner = Spinner[Int](min = 1, max = 12, initialValue = localDateTime.getMonthValue, amountToStepBy = 1)
