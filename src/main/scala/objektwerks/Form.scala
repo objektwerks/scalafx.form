e27a4d2482932ce006fd3df66739a05e1028a0b8
objektwerks <objektwerks@runbox.com>
2023-12-28 03:23:02 -0500

    experimenting with form

--- src/main/scala/objektwerks/Form.scala
+++ src/main/scala/objektwerks/Form.scala
@@ -5,10 +5,10 @@ import scalafx.scene.control.Label
 import scalafx.scene.layout.{GridPane, Priority, Region}
 
 trait Validator:
-  def validate: Boolean
+  def validate(): Boolean
 
 final class DefaultValidator extends Validator:
-  def validate: Boolean = true
+  def validate(): Boolean = true
 
 final case class Field(label: Label,
                        control: Region,
@@ -18,10 +18,20 @@ final class Form(fields: List[Field]) extends GridPane:
   hgap = 6
   vgap = 6
   padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+
   var row = 0
   for (field <- fields)
+    field.control.onInputMethodTextChanged.onChange( (source, oldValue, newValue) => validate() )
     add(field.label, columnIndex = 0, rowIndex = row)
     add(field.control, columnIndex = 1, rowIndex = row)
     row += 1
+
   GridPane.setHgrow(this, Priority.Always)
   GridPane.setVgrow(this, Priority.Always)
+
+  private def validate(): Unit =
+    var validations = 0
+    for (field <- fields)
+      if field.validator.validate() then validations += 1
+
+    // image.visible <== if validations == fields.length
