2d83bd20c02a15a15bf981071fd0094f779aeb28
objektwerks <objektwerks@runbox.com>
2023-12-29 06:07:17 -0500

    enhanced form pane

--- src/main/scala/objektwerks/FormPane.scala
+++ src/main/scala/objektwerks/FormPane.scala
@@ -18,7 +18,7 @@ final class FormPane(model: Model) extends VBox:
 
   val doubleLabel = Label("Double Field:")
   val doubleTextField = new DoubleTextField:
-    text = "1.1"
+    text = model.double.toString
 
   val comboLabel = Label("ComboBox:")
   val comboBox = new ComboBox[String]:
