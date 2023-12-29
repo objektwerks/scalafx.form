72b7f8fed38245f98b1fe3d5ef3b9d53481e5f2a
objektwerks <objektwerks@runbox.com>
2023-12-29 06:04:09 -0500

    edited model

--- src/main/scala/objektwerks/Model.scala
+++ src/main/scala/objektwerks/Model.scala
@@ -2,7 +2,7 @@ package objektwerks
 
 import java.time.LocalDateTime
 
-final case class Model(int: Int,
-                       double: Double,
-                       list: List[String],
+final case class Model(int: Int = 1,
+                       double: Double = 1.1,
+                       list: List[String] = List("abc", "def", "ghi"),
                        datetime: LocalDateTime = LocalDateTime.now)
