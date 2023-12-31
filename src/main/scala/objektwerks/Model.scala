7be4b1b4f86cca6862b31877d585b42b26e6d168
objektwerks <objektwerks@runbox.com>
2023-12-31 07:48:36 -0500

    added property to model

--- src/main/scala/objektwerks/Model.scala
+++ src/main/scala/objektwerks/Model.scala
@@ -11,4 +11,4 @@ final case class Model(int: Int = 1,
   val intProperty = ObjectProperty[Int](this, "int", int)
   val doubleProperty = ObjectProperty[Double](this, "double", double)
   val listProperty = ObjectProperty[List[String]](this, "list", list)
-
+  val datetimeProperty = ObjectProperty[LocalDateTime](this, "datetime", datetime)
