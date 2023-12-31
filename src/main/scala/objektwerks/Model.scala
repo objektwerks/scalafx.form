ddf27fdae1146496edcb602fbafe0c8ec71fa357
objektwerks <objektwerks@runbox.com>
2023-12-31 08:08:01 -0500

    enhanced model

--- src/main/scala/objektwerks/Model.scala
+++ src/main/scala/objektwerks/Model.scala
@@ -7,6 +7,7 @@ import scalafx.beans.property.ObjectProperty
 final case class Model(int: Int = 1,
                        double: Double = 1.1,
                        list: List[String] = List("abc", "def", "ghi"),
+                       listItem: String = "abc",
                        datetime: LocalDateTime = LocalDateTime.now):
   val intProperty = ObjectProperty[Int](this, "int", int)
   val doubleProperty = ObjectProperty[Double](this, "double", double)
