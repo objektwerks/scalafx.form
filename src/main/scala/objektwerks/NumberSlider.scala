5fd19c48af0d786d1c943de9243c5b1caaf0bda6
objektwerks <objektwerks@runbox.com>
2023-12-31 14:05:48 -0500

    edited number slider

--- src/main/scala/objektwerks/NumberSlider.scala
+++ src/main/scala/objektwerks/NumberSlider.scala
@@ -16,18 +16,18 @@ object NumberSlider:
 
 class NumberSlider(textFieldText: String,
                    textFieldFormatConverter: FormatStringConverter[Number]) extends HBox:
+  val slider = new Slider:
+    showTickLabels = true
+    showTickMarks = true
+    snapToTicks = true
+    minorTickCount = 0
+
   val textField = new TextField:
     style = "-fx-background-color: #ececec;"
     editable = false
     text = textFieldText
     textFormatter = new TextFormatter[Number](textFieldFormatConverter):
       value <== slider.value
-
-  val slider = new Slider:
-    showTickLabels = true
-    showTickMarks = true
-    snapToTicks = true
-    minorTickCount = 0
 
   spacing = 3
   padding = Insets(6)
