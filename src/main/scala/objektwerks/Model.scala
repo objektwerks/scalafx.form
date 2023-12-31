6479feb6789305b58793eadd4cf6f003a6137e95
objektwerks <objektwerks@runbox.com>
2023-12-31 16:48:20 -0500

    enhanced model

--- src/main/scala/objektwerks/Model.scala
+++ src/main/scala/objektwerks/Model.scala
@@ -6,12 +6,18 @@ import scalafx.beans.property.ObjectProperty
 
 final case class Model(int: Int = 5,
                        double: Double = 5.5,
+                       intRange: Range = Range(1, 10).inclusive,
+                       intRangeValue: Int = 5,
+                       doubleRange: Range = Range(1, 10).inclusive,
+                       doubleRangeValue: Double = 5.5,
                        list: List[String] = List("abc", "def", "ghi"),
                        listItem: String = "abc",
                        isSelected: Boolean = true,
                        datetime: LocalDateTime = LocalDateTime.now):
   val intProperty = ObjectProperty[Int](this, "int", int)
   val doubleProperty = ObjectProperty[Double](this, "double", double)
+  val intRangeValueProperty = ObjectProperty[Int](this, "intRangeValue", intRangeValue)
+  val doubleRangeValueProperty = ObjectProperty[Double](this, "doubleRangeValue", doubleRangeValue)
   val listItemProperty = ObjectProperty[String](this, "listItem", listItem)
   val isSelectedProperty = ObjectProperty[Boolean](this, "isSelected", isSelected)
   val datetimeProperty = ObjectProperty[LocalDateTime](this, "datetime", datetime)
