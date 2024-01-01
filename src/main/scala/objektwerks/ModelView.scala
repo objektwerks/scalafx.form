4999fe64e9e2dcb662af58ab8afec2bb19aba796
objektwerks <objektwerks@runbox.com>
2024-01-01 08:07:27 -0500

    added title to form

--- src/main/scala/objektwerks/ModelView.scala
+++ src/main/scala/objektwerks/ModelView.scala
@@ -1,8 +1,8 @@
 package objektwerks
 
 import scalafx.collections.ObservableBuffer
-import scalafx.geometry.{Insets, Pos}
-import scalafx.scene.control.{Button, CheckBox, ComboBox, Label}
+import scalafx.geometry.{Insets, Orientation, Pos}
+import scalafx.scene.control.{Button, CheckBox, ComboBox, Label, Separator}
 import scalafx.scene.layout.{Priority, VBox}
 
 import NumberSlider.*
@@ -11,6 +11,9 @@ final class ModelView(model: Model) extends VBox:
   alignment = Pos.CENTER
   padding = Insets(6)
   spacing = 6
+
+  val title = Label("~ Form ~")
+  val separator = Separator(Orientation.Horizontal)
 
   val textLabel = Label("Text Field:")
   val textField = new NonEmptyTextField:
@@ -91,5 +94,5 @@ final class ModelView(model: Model) extends VBox:
       println(s"*** Reset model: ${model.propertiesToModel}")
     }
  
-  children = List(form, submitButton)
+  children = List(title, separator, form, submitButton)
   VBox.setVgrow(this, Priority.Always)
