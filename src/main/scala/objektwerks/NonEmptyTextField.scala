94fc2b7c5b5fd3b9990de35beef945c4dc655a83
objektwerks <objektwerks@runbox.com>
2024-01-01 08:46:39 -0500

    reformatted nonempty textfield

--- src/main/scala/objektwerks/NonEmptyTextField.scala
+++ src/main/scala/objektwerks/NonEmptyTextField.scala
@@ -5,10 +5,8 @@ import scalafx.scene.control.TextFormatter.Change
 
 class NonEmptyTextField extends TextField:
   val filter: Change => Change = { (change: Change) =>
-    if change.controlNewText.length >= 1 then
-      change // if nonempty, make change
-    else
-      null // else make no change
+    if change.controlNewText.length >= 1 then change
+    else null
   }
 
   textFormatter = TextFormatter[String](filter)
