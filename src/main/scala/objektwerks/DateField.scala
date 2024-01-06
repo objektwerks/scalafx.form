73e4325db5c519aaba705973753f1e26b93a81ab
objektwerks <objektwerks@runbox.com>
2024-01-06 14:18:16 -0500

    added property to datefield

--- src/main/scala/objektwerks/DateField.scala
+++ src/main/scala/objektwerks/DateField.scala
@@ -2,9 +2,12 @@ package objektwerks
 
 import java.time.LocalDate
 
+import scalafx.beans.property.ObjectProperty
 import scalafx.geometry.Insets
 import scalafx.scene.layout.HBox
 
 class DateField(localDate: LocalDate) extends HBox:
   spacing = 3
   padding = Insets(3)
+
+  val value = ObjectProperty[LocalDate](localDate)
