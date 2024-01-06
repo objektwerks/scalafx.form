92924d60e342d3e36f982f1e1685f2beed98c757
objektwerks <objektwerks@runbox.com>
2024-01-06 14:17:27 -0500

    enhanced datefield

--- src/main/scala/objektwerks/DateField.scala
+++ src/main/scala/objektwerks/DateField.scala
@@ -2,7 +2,9 @@ package objektwerks
 
 import java.time.LocalDate
 
+import scalafx.geometry.Insets
 import scalafx.scene.layout.HBox
 
 class DateField(localDate: LocalDate) extends HBox:
-  
+  spacing = 3
+  padding = Insets(3)
