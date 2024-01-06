3c8536d503eb7b8081ac77d5ee8cafb2bd519df7
objektwerks <objektwerks@runbox.com>
2024-01-06 14:38:15 -0500

    enhanced year spinner

--- src/main/scala/objektwerks/DateField.scala
+++ src/main/scala/objektwerks/DateField.scala
@@ -26,5 +26,9 @@ class DateField(localDate: LocalDate) extends HBox:
     text = "DD:"
 
   val yearSpinner = Spinner[Int](min = localDate.getYear - 1, max = localDate.getYear, initialValue = localDate.getYear, amountToStepBy = 1)
+  yearSpinner.prefWidth = 75
+  yearSpinner.value.onChange { (_, _, newValue) => value.value = value.value.withYear(newValue) }
+
   val monthSpinner = Spinner[Int](min = 1, max = 12, initialValue = localDate.getMonthValue, amountToStepBy = 1)
+
   val daySpinner = Spinner[Int](min = 1, max = 31, initialValue = localDate.getDayOfMonth, amountToStepBy = 1)
