642a7f2ae60ee7de08cdbf81b9404eb3b438e53c
objektwerks <objektwerks@runbox.com>
2024-01-06 14:19:47 -0500

    added label to datefield

--- src/main/scala/objektwerks/DateField.scala
+++ src/main/scala/objektwerks/DateField.scala
@@ -4,6 +4,7 @@ import java.time.LocalDate
 
 import scalafx.beans.property.ObjectProperty
 import scalafx.geometry.Insets
+import scalafx.scene.control.Label
 import scalafx.scene.layout.HBox
 
 class DateField(localDate: LocalDate) extends HBox:
@@ -11,3 +12,7 @@ class DateField(localDate: LocalDate) extends HBox:
   padding = Insets(3)
 
   val value = ObjectProperty[LocalDate](localDate)
+
+  val labelYear = new Label:
+    prefHeight = 25
+    text = "YYYY:"
