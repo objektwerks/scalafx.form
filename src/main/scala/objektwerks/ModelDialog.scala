aa864bf27606c00c0841823664a235ebb377afc5
objektwerks <objektwerks@runbox.com>
2024-01-09 08:34:14 -0500

    enhanced model dialog

--- src/main/scala/objektwerks/ModelDialog.scala
+++ src/main/scala/objektwerks/ModelDialog.scala
@@ -2,5 +2,7 @@ package objektwerks
 
 import scalafx.scene.control.Dialog
 
-class ModelDialog(form: ModelForm) extends Dialog[Model]
-  
+class ModelDialog(form: ModelForm) extends Dialog[Model]:
+  initOwner(App.stage)
+  title = "Model"
+  headerText = "Model"
