0c7bc4a30c5f3a8ddf473d201122a364aaf92312
objektwerks <objektwerks@runbox.com>
2024-01-09 08:40:50 -0500

    enhanced model dialog

--- src/main/scala/objektwerks/ModelDialog.scala
+++ src/main/scala/objektwerks/ModelDialog.scala
@@ -2,7 +2,8 @@ package objektwerks
 
 import scalafx.Includes.*
 import scalafx.geometry.Orientation
-import scalafx.scene.control.{Dialog, Separator}
+import scalafx.scene.control.{ButtonType, Dialog, Separator}
+import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout.VBox
 
 class ModelDialog(form: ModelForm) extends Dialog[Model]:
@@ -12,3 +13,6 @@ class ModelDialog(form: ModelForm) extends Dialog[Model]:
 
   dialogPane().content = new VBox:
     children = List(Separator(Orientation.Horizontal), form)
+
+  val submitButtonType = new ButtonType("Submit", ButtonData.OKDone)
+  dialogPane().buttonTypes = List(submitButtonType, ButtonType.Cancel)
