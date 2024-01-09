6f096f6838769dfdf2b472d2288410f642e8e8aa
objektwerks <objektwerks@runbox.com>
2024-01-09 08:38:36 -0500

    enhanced model dialog

--- src/main/scala/objektwerks/ModelDialog.scala
+++ src/main/scala/objektwerks/ModelDialog.scala
@@ -1,8 +1,14 @@
 package objektwerks
 
-import scalafx.scene.control.Dialog
+import scalafx.Includes.*
+import scalafx.geometry.Orientation
+import scalafx.scene.control.{Dialog, Separator}
+import scalafx.scene.layout.VBox
 
 class ModelDialog(form: ModelForm) extends Dialog[Model]:
   initOwner(App.stage)
   title = "Model"
-  headerText = "Model"
+  headerText = "~ Model ~"
+
+  dialogPane().content = new VBox:
+    children = List(Separator(Orientation.Horizontal), form)
