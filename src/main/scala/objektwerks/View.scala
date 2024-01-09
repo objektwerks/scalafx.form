882239bdc5c2a04a3796ded6452b3c027385f885
objektwerks <objektwerks@runbox.com>
2024-01-09 08:56:23 -0500

    added menu to view

--- src/main/scala/objektwerks/View.scala
+++ src/main/scala/objektwerks/View.scala
@@ -3,14 +3,21 @@ package objektwerks
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
+import scalafx.scene.control.{Menu, MenuBar}
 
 class View:
-  val modelPane = ModelPane( ModelForm( Model() ) )
+  val model = Model()
+
+  val menu = Menu("Dialog")
+  val menuBar = new MenuBar():
+    menus = List(menu)
+
+  val modelPane = ModelPane( ModelForm(model) )
   
   val contentPane = new VBox:
     spacing = 6
     padding = Insets(6)
-    children = List(modelPane)
+    children = List(menuBar, modelPane)
 
   val scene = new Scene:
     root = contentPane
