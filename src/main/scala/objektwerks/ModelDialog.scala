d1364e0233a8a0f6163f138108ce67542eb03773
objektwerks <objektwerks@runbox.com>
2024-01-09 09:24:24 -0500

    refactored model dialog header

--- src/main/scala/objektwerks/ModelDialog.scala
+++ src/main/scala/objektwerks/ModelDialog.scala
@@ -2,18 +2,19 @@ package objektwerks
 
 import scalafx.Includes.*
 import scalafx.geometry.{Orientation, Pos}
-import scalafx.scene.control.{ButtonType, Dialog, Separator}
+import scalafx.scene.control.{ButtonType, Dialog, Label, Separator}
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout.VBox
 
 class ModelDialog(form: ModelForm) extends Dialog[Model]:
   initOwner(App.stage)
   title = "Model"
-  headerText = "~ Model ~"
+
+  val header = Label("~ Model ~")
 
   dialogPane().content = new VBox:
     alignment = Pos.CENTER
-    children = List(Separator(Orientation.Horizontal), form, Separator(Orientation.Horizontal))
+    children = List(header, Separator(Orientation.Horizontal), form, Separator(Orientation.Horizontal))
 
   val submitButtonType = new ButtonType("Submit", ButtonData.OKDone)
   dialogPane().buttonTypes = List(submitButtonType, ButtonType.Cancel)
