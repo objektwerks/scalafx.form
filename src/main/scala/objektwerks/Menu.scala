f0c45b4e840fc05efcd716f096198b4717c74716
objektwerks <objektwerks@runbox.com>
2024-01-10 07:28:50 -0500

    added menu

---
+++ src/main/scala/objektwerks/Menu.scala
@@ -0,0 +1,22 @@
+package objektwerks
+
+import scalafx.scene.control.{Menu, MenuBar, MenuItem, SeparatorMenuItem}
+
+object Menu:
+  def build(model: Model): MenuBar =
+    val dialogMenuItem = new MenuItem("Dialog"):
+      onAction = { _ =>
+        ModelDialog( ModelForm(model) ).showAndWait() match
+          case Some(edited: Model) => println(s"*** Model Dialog: ${edited.propertiesToModel}")
+          case _ =>
+      }
+
+    val exitMenuItem = new MenuItem("Exit"):
+      onAction = { _ => System.exit(0) }
+
+    val menu = new Menu("Menu"):
+      items = List(dialogMenuItem, SeparatorMenuItem(), exitMenuItem)
+
+    val menuBar = new MenuBar():
+      menus = List(menu)
+    menuBar
