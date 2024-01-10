9e0a4547eab30c776e04a44ac1a0e2a937a20932
objektwerks <objektwerks@runbox.com>
2024-01-10 07:32:19 -0500

    refactored app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -4,7 +4,6 @@ import scalafx.application.JFXApp3
 
 object App extends JFXApp3:
   override def start(): Unit =
-    val model = Model()
     stage = new JFXApp3.PrimaryStage:
-      scene = View(model).scene
+      scene = View( Model() ).scene
       title = "App"
