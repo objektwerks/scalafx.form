dd65a80250d650eb1bf25c5a56c55d6ac4553640
objektwerks <objektwerks@runbox.com>
2024-01-10 07:29:59 -0500

    removed menu building from view

--- src/main/scala/objektwerks/View.scala
+++ src/main/scala/objektwerks/View.scala
@@ -3,25 +3,9 @@ package objektwerks
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
-import scalafx.scene.control.{Menu, MenuBar, MenuItem, SeparatorMenuItem}
 
 final class View(model: Model):
-  val dialogMenuItem = new MenuItem("Dialog"):
-    onAction = { _ =>
-      ModelDialog( ModelForm(model) ).showAndWait() match
-        case Some(edited: Model) => println(s"*** Model Dialog: ${edited.propertiesToModel}")
-        case _ =>
-    }
-
-  val exitMenuItem = new MenuItem("Exit"):
-    onAction = { _ => System.exit(0) }
-
-  val menu = new Menu("Menu"):
-    items = List(dialogMenuItem, SeparatorMenuItem(), exitMenuItem)
-
-  val menuBar = new MenuBar():
-    menus = List(menu)
-
+  val menuBar = Menu.build(model)
   val modelPane = ModelPane( ModelForm(model) )
   
   val contentPane = new VBox:
