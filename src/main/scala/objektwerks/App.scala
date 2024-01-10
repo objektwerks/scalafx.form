dae15725c83d40a3e408683d886c641726b41ce5
objektwerks <objektwerks@runbox.com>
2024-01-10 07:24:58 -0500

    moved model to app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -4,6 +4,7 @@ import scalafx.application.JFXApp3
 
 object App extends JFXApp3:
   override def start(): Unit =
+    val model = Model()
     stage = new JFXApp3.PrimaryStage:
-      scene = View().scene
+      scene = View(model).scene
       title = "App"
