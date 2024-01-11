53996fade5a691260282c4b1c81cb2a612cb7796
objektwerks <objektwerks@runbox.com>
2024-01-11 08:39:41 -0500

    maded double text field final

--- src/main/scala/objektwerks/DoubleTextField.scala
+++ src/main/scala/objektwerks/DoubleTextField.scala
@@ -7,12 +7,14 @@ import scalafx.util.converter.DoubleStringConverter
 object DoubleTextField:
   val regex = """([0-9]+([.][0-9]*)?|[.][0-9]+)""".r
 
-class DoubleTextField extends TextField: 
+final class DoubleTextField extends TextField:
   val converter = DoubleStringConverter()
+
   val filter: Change => Change = { (change: Change) =>
     if DoubleTextField.regex.matches(change.controlNewText) then change
     else null
   }
+
   textFormatter = TextFormatter[Double](converter, 0.0, filter)
 
   def double(default: Double): Double = text.value.toDoubleOption.getOrElse(default)
