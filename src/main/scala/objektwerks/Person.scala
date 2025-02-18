1093332b897c34d940c69ca6662be161bbbcaf9d
objektwerks <objektwerks@runbox.com>
2025-02-18 10:21:02 -0500

    enhanced person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -1,8 +1,19 @@
 package objektwerks
 
-import scalafx.beans.property.ObjectProperty
 import scalafx.beans.binding.Bindings
+import scalafx.beans.property.ObjectProperty
+import scalafx.scene.control.{Label, TextField}
+import scalafx.scene.layout.VBox
 
 final case class Person(name: String):
   val nameProperty = ObjectProperty(name)
   nameProperty.bind( Bindings.createObjectBinding(() => this.name, nameProperty) )
+
+
+final class PersonForm(person: Person) extends VBox:
+  val personProperty = ObjectProperty(person)
+
+  val labelName = Label("Name" )
+
+  val textFieldName = new TextField():
+    text <==> personProperty.value.nameProperty
