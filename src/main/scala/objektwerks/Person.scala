d12f293a84e745d21eb9c26eeedfd187ce7fce09
objektwerks <objektwerks@runbox.com>
2025-02-18 11:46:30 -0500

    edited person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -1,9 +1,6 @@
 package objektwerks
 
-// import scalafx.beans.binding.Bindings
 import scalafx.beans.property.StringProperty
 
 final case class Person(name: String):
   val nameProperty = StringProperty(name)
-  // throws bound value cannot be set exception
-  // nameProperty.bind( Bindings.createObjectBinding(() => this.name, nameProperty) )
