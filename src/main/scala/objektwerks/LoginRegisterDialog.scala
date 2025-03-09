77aa29125842e22a056f1c030900dc0308f13de7
objektwerks <objektwerks@runbox.com>
2025-03-09 15:11:43 -0400

    edited login register dialog

--- src/main/scala/objektwerks/LoginRegisterDialog.scala
+++ src/main/scala/objektwerks/LoginRegisterDialog.scala
@@ -10,10 +10,10 @@ final case class Login(emailAddress: String, pin: String)
 final case class Register(emailAddress: String)
 final case class LoginRegister(login: Option[Login], register: Option[Register])
 
-class LoginRegisterDialog(stage: Stage,
-                          title: String,
-                          headerText: String,
-                          graphic: ImageView) extends Dialog:
+final class LoginRegisterDialog(stage: Stage,
+                                title: String,
+                                headerText: String,
+                                graphic: ImageView) extends Dialog:
   initOwner(stage)
   title = title
   headerText = headerText
