493a21ea081c701eb297d8506be883bf6a2c6fa1
objektwerks <objektwerks@runbox.com>
2025-03-09 16:43:46 -0400

    added validator

---
+++ src/main/scala/objektwerks/Validator.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+object Validator:
+  extension (value: String)
+    def isPin: Boolean = value.length == 7
+    def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
+    def isInt(text: String): Boolean = text.matches("\\d+")
+    def isDouble(text: String): Boolean = text.matches("\\d{0,7}([\\.]\\d{0,4})?")
