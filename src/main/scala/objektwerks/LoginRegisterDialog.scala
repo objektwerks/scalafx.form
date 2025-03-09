c7d4c6d4e8340af2cf6a294082a7c89539c5314d
objektwerks <objektwerks@runbox.com>
2025-03-09 07:50:12 -0400

    enhanced login register dialog

--- src/main/scala/objektwerks/LoginRegisterDialog.scala
+++ src/main/scala/objektwerks/LoginRegisterDialog.scala
@@ -1,5 +1,7 @@
 package objektwerks
 
 import scalafx.scene.control.Dialog
+import scalafx.stage.Stage
 
-class LoginRegisterDialog extends Dialog
+class LoginRegisterDialog(stage: Stage) extends Dialog:
+  initOwner(stage)
